201d65c121ff40540676c9f620a0a7f0ea5b6912
Andy Edwards <jedwards@fastmail.com>
2015-07-29 00:23:43 -0500

    damn appcache won't work with local files

--- build.sh
+++ build.sh
@@ -15,29 +15,13 @@ do
   md5=$1
   ext=${file##*.}
   base=${file%.*}
-  cp $file build/web/${base}-${md5}.${ext}
-  echo https://frcs.xyz/notes/${base}-${md5}.${ext} >> build/web/frcs-notes.appcache
-  perl -pi -e "s|${file}|${base}-${md5}.${ext}|g" build/web/index.html
+  newname=${base}-${md5}.${ext}
+  cp $file build/web/$newname
+  echo $newname >> build/web/frcs-notes.appcache
+  perl -pi -e "s|${file}|${newname}|g" build/web/index.html
 done
 
 for file in assets/*
 do
-  [ "${file##*.}" != "js" ] && echo https://frcs.xyz/notes/${file} >> build/web/frcs-notes.appcache
-done
-
-mkdir -p build/frcs-notes-local
-
-cp -urvp build/web/frcs-notes.appcache build/frcs-notes-local
-cp config-local.js build/frcs-notes-local/config.js
-cp index.html build/frcs-notes-local
-
-perl -pi -e "s|frcs-notes.appcache|https://frcs.xyz/notes/frcs-notes.appcache|g" build/frcs-notes-local/index.html
-
-for file in data.js assets/bundle.js
-do
-  set -- $(md5sum $file)
-  md5=$1
-  ext=${file##*.}
-  base=${file%.*}
-  perl -pi -e "s|${file}|https://frcs.xyz/notes/${base}-${md5}.${ext}|g" build/frcs-notes-local/index.html
+  [ "${file##*.}" != "js" ] && echo ${file} >> build/web/frcs-notes.appcache
 done
